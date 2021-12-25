47619deb4b2578d68e6f06588b3a85b357eebdd3
AlexanderP <kennedyisdead13@gmail.com>
2021-12-25 01:02:10 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pr4j';
+
+
+
+  // public readonly VAPID_PUBLIC_KEY = 'BMx-2DwKU-jh_Ndf82RP4TR3cH9IbVcbsmuZey1Yhla8BeZ1nTi3FhkkkRKwBqMZ-rTbp0iPXZf3egMwiNNJjUo'
+
+  // constructor(private swPush : SwPush , private apiRest: ApiRestService){
+
+  //   this.subscribeToNotifications();
+  // }
 }
